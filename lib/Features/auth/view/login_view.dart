b8a01fda2512b420b4bf2f57ca754482e9f1d9e2
Om Mishra <om.works01@gmail.com>
2023-04-14 15:33:39 +0530

    Created the Login Fields

--- lib/Features/auth/view/login_view.dart
+++ lib/Features/auth/view/login_view.dart
@@ -1,7 +1,6 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:todoapp/Constants/ui_constants.dart';
+import 'package:todoapp/Features/auth/widgets/auth_field.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -11,10 +10,42 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final appbar = UIConstants.appBar();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: UIConstants.appBar(),
+      appBar: appbar,
+      body: Center(
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(children: [
+            //textfield1
+            AuthField(
+              controller: emailController,
+              hintText: "E-mail",
+            ),
+            const SizedBox(height: 25),
+            //textfield2
+            AuthField(
+              controller: passwordController,
+              hintText: "Password",
+            ),
+            //button
+            //textspan
+          ]),
+        )),
+      ),
     );
   }
 }
