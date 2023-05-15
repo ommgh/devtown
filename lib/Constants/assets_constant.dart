20d4b2ccc732e8bf4d4d2a89dd53e5a1947a3674
Om Mishra <om.works01@gmail.com>
2023-05-15 22:11:25 +0530

    Update assets_constant.dart

--- lib/Constants/assets_constant.dart
+++ lib/Constants/assets_constant.dart
@@ -1,5 +1,5 @@
 class AssetsConstants {
   static const String _svgPath = "assets/svgs";
-  static const String logo = "$_svgPath/todo-list-svgrepo-com.svg";
+  static const String logo = "$_svgPath/twitter.svg";
   static const String googlelogo = "$_svgPath/Google-Logo.svg";
 }
