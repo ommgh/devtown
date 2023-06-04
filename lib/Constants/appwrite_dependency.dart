d113259cfce858be09fad88510bcb5b2fa01321c
Om Mishra <om.works01@gmail.com>
2023-06-04 16:24:51 +0530

    Updated endpoint

--- lib/Constants/appwrite_dependency.dart
+++ lib/Constants/appwrite_dependency.dart
@@ -1,5 +1,5 @@
 class AppwriteContants {
-  static const String dtatabaseID = "6437c852d32fb6bad8e9";
-  static const String projectID = "643792182033890216d6";
-  static const String endPoint = "192.168.225.1/Om Mishra";
+  static const String dtatabaseID = "647c6bc3d7f483aa9967";
+  static const String projectID = "647c697a8ea533486648";
+  static const String endPoint = "https://cloud.appwrite.io/v1";
 }
