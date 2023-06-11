20961fa3e679ccacda43fe05a932263d8a52ba74
Om Mishra <om.works01@gmail.com>
2023-06-11 14:18:57 +0530

    Uploading Images

--- lib/Features/post/controllers/post_controller.dart
+++ lib/Features/post/controllers/post_controller.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:todoapp/Features/auth/controller/auth_controller.dart';
 import 'package:todoapp/api/post_api.dart';
+import 'package:todoapp/api/storage_api.dart';
 import 'package:todoapp/core/enums/post_type.dart';
 import 'package:todoapp/core/ustils.dart';
 import 'package:todoapp/models/post_model.dart';
@@ -14,18 +15,22 @@ final postControllerProvider = StateNotifierProvider<PostController, bool>(
     return PostController(
       ref: ref,
       postAPI: ref.watch(postAPIProvider),
+      storageAPI: ref.watch(storageAPIProvider),
     );
   },
 );
 
 class PostController extends StateNotifier<bool> {
   final PostAPI _postAPI;
+  final StorageAPI _storageAPI;
   final Ref _ref;
   PostController({
     required Ref ref,
     required PostAPI postAPI,
+    required StorageAPI storageAPI,
   })  : _ref = ref,
         _postAPI = postAPI,
+        _storageAPI = storageAPI,
         super(false);
 
   void sharePost({
@@ -56,7 +61,29 @@ class PostController extends StateNotifier<bool> {
     required List<File> images,
     required String text,
     required BuildContext context,
-  }) {}
+  }) async {
+    state = true;
+    final hashtags = _getHastagFromtText(text);
+    String link = _getLinkFromText(text);
+    final user = _ref.read(currentUserDetailsProvider).value!;
+    final imageLinks = await _storageAPI.uploadImage(images);
+    Post post = Post(
+      text: text,
+      hashtags: hashtags,
+      link: link,
+      imageLinks: imageLinks,
+      uid: user.uid,
+      postType: PostType.image,
+      postedAt: DateTime.now(),
+      likes: const [],
+      commentIds: const [],
+      id: '',
+      reshareCount: 0,
+    );
+    final res = await _postAPI.sharePost(post);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) => null);
+  }
 
   void _shareTextPost({
     required String text,
