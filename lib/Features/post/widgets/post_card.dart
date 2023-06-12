950f1bb6e6fc6748467f34e04c5f46b65c1ba10d
Om Mishra <om.works01@gmail.com>
2023-06-12 16:11:17 +0530

    Preview Links

--- lib/Features/post/widgets/post_card.dart
+++ lib/Features/post/widgets/post_card.dart
@@ -1,8 +1,14 @@
+import 'package:any_link_preview/any_link_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todoapp/Features/auth/controller/auth_controller.dart';
+import 'package:todoapp/Features/post/widgets/carousel_image.dart';
+import 'package:todoapp/Features/post/widgets/hashtag_text.dart';
 import 'package:todoapp/common/common.dart';
+import 'package:todoapp/core/enums/post_type.dart';
 import 'package:todoapp/models/post_model.dart';
+import 'package:todoapp/theme/pallet.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class PostCard extends ConsumerWidget {
   final Post post;
@@ -18,6 +24,7 @@ class PostCard extends ConsumerWidget {
             return Column(
               children: [
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
                       margin: const EdgeInsets.all(10),
@@ -26,24 +33,46 @@ class PostCard extends ConsumerWidget {
                         radius: 25,
                       ),
                     ),
-                    Column(
-                      children: [
-                        //Reshare
-                        Row(
-                          children: [
-                            Container(
-                              margin: const EdgeInsets.only(right: 5),
-                              child: Text(
-                                user.name,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 19,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //Reshare
+                          Row(
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(right: 5),
+                                child: Text(
+                                  user.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 19,
+                                  ),
                                 ),
                               ),
-                            )
-                          ],
-                        )
-                      ],
+                              Text(
+                                '@${user.name} . ${timeago.format(
+                                  post.postedAt,
+                                  locale: 'en_short',
+                                )}',
+                                style: const TextStyle(
+                                  color: Pallete.greyColor,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          //repliedto
+                          HashtagText(text: post.text),
+                          if (post.postType == PostType.image)
+                            CarouselImage(imageLinks: post.imageLinks),
+                          if (post.link.isNotEmpty) ...[
+                            const SizedBox(height: 4),
+                            AnyLinkPreview(link: 'https://${post.link}'),
+                          ]
+                        ],
+                      ),
                     ),
                   ],
                 ),
