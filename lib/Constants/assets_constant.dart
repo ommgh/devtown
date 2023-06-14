da10871640be53e09695f13bf37f2fee9b9f6764
Om Mishra <om.works01@gmail.com>
2023-06-14 23:53:55 +0530

    Fixed Hashtag Query

--- lib/Constants/assets_constant.dart
+++ lib/Constants/assets_constant.dart
@@ -1,6 +1,6 @@
 class AssetsConstants {
   static const String _svgPath = "assets/svgs";
-  static const String logo = "$_svgPath/twitter.svg";
+  static const String logo = "$_svgPath/devlogo.svg";
   static const String googlelogo = "$_svgPath/Google-Logo.svg";
   static const String homeOutlined = "$_svgPath/home_outlined.svg";
   static const String homeFilled = "$_svgPath/home_filled.svg";
